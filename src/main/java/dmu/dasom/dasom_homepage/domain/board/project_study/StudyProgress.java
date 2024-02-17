baa0255ecc64eae90d4aa333505861901ffdc1c6
sinyoung0110 <dpqlc7008@naver.com>
2024-02-17 16:39:45 +0900

    깃 충돌

---
+++ src/main/java/dmu/dasom/dasom_homepage/domain/board/project_study/StudyProgress.java
@@ -0,0 +1,7 @@
+package dmu.dasom.dasom_homepage.domain.board.project_study;
+
+public class StudyProgress {
+    private int study_no;
+    private int study_week;
+    private String week_content;
+}
