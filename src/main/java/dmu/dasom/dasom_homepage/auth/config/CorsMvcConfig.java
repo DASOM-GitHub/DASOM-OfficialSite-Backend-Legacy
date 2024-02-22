1b3b3c14221b8454c30f83e824b87d0178e67965
Seungwan Yoo <ysw99@kakao.com>
2024-02-22 18:37:53 +0900

    API 테스트용 리소스 접근 제한 해제

--- src/main/java/dmu/dasom/dasom_homepage/auth/config/CorsMvcConfig.java
+++ src/main/java/dmu/dasom/dasom_homepage/auth/config/CorsMvcConfig.java
@@ -9,6 +9,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+//                .allowedOrigins("http://localhost:3000");
+                .allowedOriginPatterns("*");
     }
 }
